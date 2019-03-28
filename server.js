c6dfe39aeaed1bf4b6fbc6b30f396dd083870737
DrewDrizzle <aramouni8@gmail.com>
2019-03-27 20:44:10 -0400

    added server for heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "NYT-Google-Books-Search/creat-react-express/client/public/index.html"));
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
+  
